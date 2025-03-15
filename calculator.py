6c1c8bc636b894747479a68a96169731a4a344b7
jepkorir433 <sharonshah2006@gmail.com>
2025-03-15 04:54:07 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+number1 = input('Enter the num')
+num1 = int(number1)
+sign = input('')
+number2 = inout('Enter the number')
+num2 = int(number2)
+if operation ==  '1':
+        print('The sum is '  + str(num1 +num2) 
+elif operation== '2':
+         print('The sum is ' + str(num1 -num2) 
+               
+elif  operation== '3':
+        print('The sum is'  + str(num1 *num2)
+elif operation== "4":
+        print('The sum is'+ str(num1 /num2)
+            
+
+else:
+        print('please enter + , -, * or /')
+        
